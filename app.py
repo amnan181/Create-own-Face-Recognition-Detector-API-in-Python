4617fb4255f5f52efae21fbda202eb449449c87e
amnan181 <sweetrajput181@gmail.com>
2018-12-28 20:50:08 +0500

    Succefuly Created Face Recognition API

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask,jsonify,request
+from flask_cors import CORS
+import os
+import face_recognition
+app = Flask(__name__)
+images = os.listdir('images')
+CORS(app)
+
+@app.route('/')
+def index():
+    return 'Server Succesfuly Running!'
+
+@app.route('/upload',methods=['GET','POST'])
+def upload():
+    image = request.files['file']
+    image_to_be_matched = face_recognition.load_image_file(image)
+    image_to_be_matched_encoded = face_recognition.face_encodings(
+    image_to_be_matched)[0]
+    for image in images:
+        current_image = face_recognition.load_image_file("images/" + image)
+        current_image_encoded = face_recognition.face_encodings(current_image)[0]
+        result = face_recognition.compare_faces(
+            [image_to_be_matched_encoded], current_image_encoded)
+        if result[0] == True:
+            return jsonify({'success':'true',"Matched" : image.rsplit('.')[0]})
+    return jsonify({'success':'false'})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",port=2020,debug=True)
